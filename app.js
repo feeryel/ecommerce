31d1922c11494d49236153a36c78681d6aab8933
feryel dadi <ferydadi796@gmail.com>
2025-12-18 10:18:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const mongoose =require("mongoose")
+const dotenv =require('dotenv')
+const cors = require('cors')
+const app = express();
+const categorieRouter =require("./routes/categorie.route")
+const scategorieRouter =require("./routes/scategorie.route")
+const articleRouter =require("./routes/article.route")
+app.use(express.json());
+
+app.use('/api/categories', categorieRouter);
+app.use('/api/scategories', scategorieRouter);
+app.use('/api/articles', articleRouter);
+//config dotenv
+dotenv.config()
+//Les cors
+app.use(cors())
+//BodyParser Middleware
+// Connexion à la base données
+mongoose.connect(process.env.DATABASECLOUD)
+.then(() => {console.log("DataBase Successfully Connected");})
+.catch(err => { console.log("Unable to connect to database", err);
+process.exit(); });
+// requête
+app.get("/",(req,res)=>{
+res.send("bonjour");
+
+});
+app.listen(process.env.PORT, () => {
+console.log(`Server is listening on port ${process.env.PORT}`); });
+module.exports = app;
+
